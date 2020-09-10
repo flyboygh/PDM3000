67709a0562f340e91d66149449b9270576e9c96c
wy <846053959@qq.com>
2020-09-10 17:47:01 +0800

    支持无线列表显示

--- app/src/main/java/com/sindia/pdm3000/MainActivity.java
+++ app/src/main/java/com/sindia/pdm3000/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sindia.pdm3000;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
@@ -13,7 +14,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.wifi.WifiInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -22,25 +26,40 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.sindia.pdm3000.adapter.BleDeviceAdapter;
+import com.sindia.pdm3000.adapter.WifiAdapter;
 import com.sindia.pdm3000.ble.IBleDeviceScan;
 import com.sindia.pdm3000.ble.BleManager;
 import com.sindia.pdm3000.util.BluetoothUtil;
 import com.sindia.pdm3000.util.LocationUtil;
+import com.sindia.pdm3000.util.WifiAdmin;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, BleDeviceAdapter.Callback {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, BleDeviceAdapter.Callback, WifiAdapter.Callback {
     // 常量
     private static final String TAG = "MainActivity";
     // 状态相关的
     private boolean mScanning = false;
-    // 控件相关的
-    private ListView mListView = null;
     // 蓝牙相关的
-    private BluetoothUtil _BluetoothUtil = null;
+    private ListView mDeviceListView = null;
     private BleDeviceAdapter mDeviceAdapter;
+    private BluetoothUtil _BluetoothUtil = null;
     private BluetoothGatt mBleGatt = null;
     private BluetoothStateBroadcastReceive mBluetoothReceive;
+    // 无线网相关的
+    private ListView mWifiListView = null;
+    private WifiAdapter mWifiAdapter;
+    private WifiAdmin wiFiAdmin;
+    private List<android.net.wifi.ScanResult> mWifiList;
+
+    //private WifiManager mWifiManager = null;
+    private WifiInfo wifiInfo = null;       //获得的Wifi信息
+    private Handler handler;
+    private int level;
+    private String macAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,20 +98,128 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         // 设备列表相关
         mDeviceAdapter = new BleDeviceAdapter(this, this);
-        mListView = findViewById(R.id.listviewDevices);
-        mListView.setOnItemClickListener(this);
-        mListView.setAdapter(mDeviceAdapter);
-
-        UpdateActivityControls();
+        mDeviceListView = findViewById(R.id.listviewDevices);
+        mDeviceListView.setOnItemClickListener(this);
+        mDeviceListView.setAdapter(mDeviceAdapter);
 
         // 设备扫描结果回调
         BleManager.getInstance().bleDeviceScan = new IBleDeviceScan() {
             @Override
             public void onBleDeviceChanged(ArrayList<ScanResult> deviceList) {
                 mDeviceAdapter.mScanList = deviceList;
-                mListView.setAdapter(mDeviceAdapter);
+                mDeviceListView.setAdapter(mDeviceAdapter);
             }
         };
+
+        // 无线网相关的
+        mWifiAdapter = new WifiAdapter(this, this);
+        mWifiListView = findViewById(R.id.listviewWifis);
+        mWifiListView.setOnItemClickListener(this);
+        mWifiListView.setAdapter(mWifiAdapter);
+
+        wiFiAdmin = new WifiAdmin(this);
+        wiFiAdmin.openWifi();
+
+        // 判断wifi是否开启
+        /*mWifiManager = (WifiManager)getApplicationContext().getSystemService(WIFI_SERVICE);
+        if (!mWifiManager.isWifiEnabled()) {
+            if(mWifiManager.getWifiState() != WifiManager.WIFI_STATE_ENABLING) {
+                mWifiManager.setWifiEnabled(true);
+            }
+        }*/
+        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {//如果 API level 是大于等于 23(Android 6.0) 时
+            //判断是否具有权限
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                //判断是否需要向用户解释为什么需要申请该权限
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                    //showToast("自Android 6.0开始需要打开位置权限");
+                }
+                //请求权限
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        REQUEST_CODE_ACCESS_COARSE_LOCATION);
+            }
+        }*/
+// 使用定时器,每隔5秒获得一次信号强度值
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                wiFiAdmin.startScan();
+                mWifiList = wiFiAdmin.getWifiList();
+
+                List<android.net.wifi.ScanResult> listb = mWifiList;//mWifiManager.getScanResults();
+                Log.d("wifi","listb"+listb);
+                Log.d("wifi","listb.size()"+listb.size());
+                //数组初始化要注意
+                String[] listSSID = new String[listb.size()];
+                String[] listBSSID = new String[listb.size()];
+                int[] listLevel = new int[listb.size()];
+                if (listb != null) {
+                    for (int i = 0; i < listb.size(); i++) {
+                        android.net.wifi.ScanResult scanResult = listb.get(i);
+                        listSSID[i] = scanResult.SSID;
+                        listBSSID[i] = scanResult.BSSID;
+                        listLevel[i] = scanResult.level;
+                    }
+                }
+                String[] listSSID0 = new String[listb.size()];
+                String[] listBSSID0 = new String[listb.size()];
+                int[] listLevel0 = new int[listb.size()];
+                if (listb == null) {
+                    listSSID0[0] = "NoWiFi";
+                    listBSSID0[0] = "NoWiFi";
+                    listLevel[0] = -200; //-200默认没有wifi
+                } else {
+                    listSSID0 = listSSID;
+                    listBSSID0 = listBSSID;
+                    listLevel0 = listLevel;
+                }
+                for (int i = 0; i < listb.size(); i++) {
+                    Log.d("wifi", listSSID0[i] + "//" + listBSSID0[i] + "//" + listLevel0[i]);
+                }
+                Log.d("wifi", "=======================");
+                wifiInfo = wiFiAdmin.GetWifiManager().getConnectionInfo();//  mWifiManager.getConnectionInfo();
+                //获得信号强度值
+                level = wifiInfo.getRssi();
+                macAddress = wifiInfo.getBSSID();
+                Message msg = new Message();
+                handler.sendMessage(msg);
+            }
+
+        }, 1000, 5000);
+        // 使用Handler实现UI线程与Timer线程之间的信息传递,每5秒告诉UI线程获得wifiInto
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                Toast.makeText(MainActivity.this,
+                        "信号强度：" + level + "  BSSID: " + macAddress, Toast.LENGTH_SHORT)
+                        .show();
+                mWifiAdapter.mScanList = mWifiList;
+                mWifiListView.setAdapter(mWifiAdapter);
+            }
+        };
+
+        // 更新控件显赫
+        UpdateActivityControls();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        /*if (requestCode == REQUEST_CODE_ACCESS_COARSE_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //用户允许改权限，0表示允许，-1表示拒绝 PERMISSION_GRANTED = 0， PERMISSION_DENIED = -1
+                //permission was granted, yay! Do the contacts-related task you need to do.
+                //这里进行授权被允许的处理
+            } else {
+                //permission denied, boo! Disable the functionality that depends on this permission.
+                //这里进行权限被拒绝的处理
+            }
+        } else {*/
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //}
     }
 
     // 根据功能状态，更新控件状态
@@ -245,7 +372,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             //}
         }
         mDeviceAdapter.mConnDevice = device;
-        mListView.setAdapter(mDeviceAdapter);
+        mDeviceListView.setAdapter(mDeviceAdapter);
     }
 
     // 蓝牙状态接收
--- app/src/main/java/com/sindia/pdm3000/adapter/WifiAdapter.java
+++ app/src/main/java/com/sindia/pdm3000/adapter/WifiAdapter.java
@@ -1,26 +1,25 @@
 package com.sindia.pdm3000.adapter;
 
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.le.ScanResult;
+import android.net.wifi.ScanResult;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.sindia.pdm3000.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class BleDeviceAdapter extends BaseAdapter implements View.OnClickListener {
+public class WifiAdapter extends BaseAdapter implements View.OnClickListener {
     private Context mContext = null;
     private Callback mCallback = null;
-    public BluetoothDevice mConnDevice = null;
-    public ArrayList<ScanResult> mScanList = new ArrayList<>();
+    public List<ScanResult> mScanList = new ArrayList<>();
 
-    public BleDeviceAdapter(Context context, Callback callback) {
+    public WifiAdapter(Context context, Callback callback) {
         mContext = context;
         mCallback = callback;
     }
@@ -46,11 +45,16 @@ public class BleDeviceAdapter extends BaseAdapter implements View.OnClickListene
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        view = LayoutInflater.from(mContext).inflate(R.layout.ble_device_cell, viewGroup,false);
+        view = LayoutInflater.from(mContext).inflate(R.layout.wifi_cell, viewGroup,false);
         TextView txt_aName = view.findViewById(R.id.textViewDeviceName);
-        String s = mScanList.get(i).getScanRecord().getDeviceName();
-        txt_aName.setText(s);
-        Button btnConnect = view.findViewById(R.id.buttonConnect);
+        ScanResult s_result = mScanList.get(i);
+        String s = s_result.SSID;
+        if (s != null) {
+            txt_aName.setText(s);
+        } else {
+            Log.d("", "");
+        }
+        /*Button btnConnect = view.findViewById(R.id.buttonConnect);
         if (mConnDevice != null) {
             if (mConnDevice.equals(mScanList.get(i).getDevice())) {
                 btnConnect.setText(R.string.disconn);
@@ -63,13 +67,12 @@ public class BleDeviceAdapter extends BaseAdapter implements View.OnClickListene
             btnConnect.setVisibility(View.VISIBLE);
         }
         btnConnect.setOnClickListener(this);
-        btnConnect.setTag(i);
+        btnConnect.setTag(i);*/
         return view;
     }
 
     @Override
     public void onClick(View view) {
-        int index = (int)view.getTag();
-        mCallback.connectClick(index);
+
     }
 }
